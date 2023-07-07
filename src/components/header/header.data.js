927a4d6d0fedacbb843a272dd19e3b45888ecf93
stephen6611 <stephenandreas04@gmail.com>
2023-07-07 09:38:37 +0700

    1st

---
+++ src/components/header/header.data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: 'banner',
+    label: 'Beranda',
+  },
+  {
+    path: 'services',
+    label: 'Layanan',
+  },
+  {
+    path: 'pricing',
+    label: 'Harga',
+  },
+  {
+    path: 'testimonials',
+    label: 'Testimoni',
+  },
+  {
+    path: 'news',
+    label: 'Galery',
+  },
+];
